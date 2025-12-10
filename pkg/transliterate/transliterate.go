53e1d021dcc17c6cb6a594df4db197d4b29461d6
MB <mb@m1k.pw>
2025-12-10 12:24:02 +0000

    Add Devanagari digit transliteration to Arabic numerals

--- pkg/transliterate/transliterate.go
+++ pkg/transliterate/transliterate.go
@@ -55,6 +55,9 @@ var devaToIAST = map[rune]string{
 	'श': "ś", 'ष': "ṣ", 'स': "s", 'ह': "h",
 	// Avagraha
 	'ऽ': "'",
+	// Devanagari digits
+	'०': "0", '१': "1", '२': "2", '३': "3", '४': "4",
+	'५': "5", '६': "6", '७': "7", '८': "8", '९': "9",
 }
 
 // Devanagari mātrās to IAST vowels
